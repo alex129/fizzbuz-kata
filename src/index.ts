9ebdde6fd8200ae9a37da0e7745e72ca6ee3a328
Plk <plikforo@gmail.com>
2024-01-02 17:28:31 +0100

    refactor: :recycle: Refactor fizzbuzz to return number as literal string

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export const fizzbuzz = (number: number): string => {
-  if (number === 1) {
-    return '1';
-  } else if (number === 2) {
-    return '2';
-  } else {
-    return '4';
-  }
+  return number.toString();
 };
