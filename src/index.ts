6e439af18ac475ee1e78635c4e58020a005240e1
Plk <plikforo@gmail.com>
2024-01-09 13:11:09 +0100

    test: :white_check_mark: When I fizzbuzz number 15, I get back "FizzBuzz"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export const fizzbuzz = (number: number): string => {
-  if (number % 3 === 0) return 'Fizz';
+  if (number === 15) return 'FizzBuzz';
+  else if (number % 3 === 0) return 'Fizz';
   else if (number % 5 === 0) return 'Buzz';
-  return number.toString();
+  else return number.toString();
 };
