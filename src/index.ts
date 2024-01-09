b8964c76ba99d50b71d66f2f89e6573b18d430ff
Plk <plikforo@gmail.com>
2024-01-09 13:07:28 +0100

    refactor: :recycle: Refactor fizzbuzz to return Buzz when the input number is a multiple of 5

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export const fizzbuzz = (number: number): string => {
   if (number % 3 === 0) return 'Fizz';
-  else if (number === 5 || number === 10) return 'Buzz';
+  else if (number % 5 === 0) return 'Buzz';
   return number.toString();
 };
